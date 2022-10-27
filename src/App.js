603ed6c91450abee35af33f0b4651501f1c8d5c1
qamraaan <qamraaan@gmail.com>
2022-10-27 22:05:26 +0530

    search...

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-import { useSearchParams } from "react-router-dom";
 
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
 function App() {
-  const [searchParams] = useSearchParams();
+  // }, []);
 
-  useEffect(() => {
-    console.log(searchParams.get("code"));
-  }, []);
-  let url = `https://api.instagram.com/oauth/authorize?client_id=1096816224277958&redirect_uri=https://instadetails.netlify.app/&scope=user_profile,user_media&response_type=code`;
   // const getData = async () => {
   //   const response = await fetch();
   //   // `/oauth/authorize?client_id=1096816224277958&redirect_uri=https://instadetails.netlify.app/&scope=user_profile,user_media&response_type=code`
@@ -17,13 +14,16 @@ function App() {
   //   console.log(response);
   // };
   return (
-    <div className="App">
-      <h1>Hola amigos!</h1>
-      <a href={url}>Add Button</a>
-      {/* <button className="btn" onClick={getData}>
-        Add Account
-      </button> */}
-    </div>
+    <Router>
+      <div className="App">
+        <Link to="https://api.instagram.com/oauth/authorize?client_id=1096816224277958&redirect_uri=https://instadetails.netlify.app/&scope=user_profile,user_media&response_type=code">
+          Click
+        </Link>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 

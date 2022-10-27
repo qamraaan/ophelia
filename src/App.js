dfd2910e586e2e7b570e4b16053aba8790f2cd00
qamraaan <qamraaan@gmail.com>
2022-10-27 21:25:22 +0530

    used search params

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import { useSearchParams } from "react-router-dom";
 
 function App() {
-  // useEffect(() => {
-  //   getData();
-  // }, []);
+  const [searchParams] = useSearchParams();
 
-  const getData = async () => {
-    const response = await fetch(
-      `/oauth/authorize?client_id=1096816224277958&redirect_uri=https://instadetails.netlify.app/&scope=user_profile,user_media&response_type=code`
-      // `https://e508-137-59-1-12.in.ngrok.io`
-    );
-    const data = await response.json();
-    console.log(data);
-  };
+  useEffect(() => {
+    console.log(searchParams.get("code"));
+  }, []);
+  let url = `https://api.instagram.com/oauth/authorize?client_id=1096816224277958&redirect_uri=https://instadetails.netlify.app/&scope=user_profile,user_media&response_type=code`;
+  // const getData = async () => {
+  //   const response = await fetch();
+  //   // `/oauth/authorize?client_id=1096816224277958&redirect_uri=https://instadetails.netlify.app/&scope=user_profile,user_media&response_type=code`
+  //   // `https://e508-137-59-1-12.in.ngrok.io`
+  //   // const data = await response.json();
+  //   console.log(response);
+  // };
   return (
     <div className="App">
       <h1>Hola amigos!</h1>
-      <button className="btn" onClick={getData}>
+      <a href={url}>Add Button</a>
+      {/* <button className="btn" onClick={getData}>
         Add Account
-      </button>
+      </button> */}
     </div>
   );
 }

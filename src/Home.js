83540a66fbb88c071b5839a8819d449be632f714
qamraaan <qamraaan@gmail.com>
2022-10-28 11:45:39 +0530

    modify functionality

--- src/Home.js
+++ src/Home.js
@@ -5,32 +5,32 @@ const Home = () => {
   let url = `https://api.instagram.com/oauth/authorize?client_id=1096816224277958&redirect_uri=https://instadetails.netlify.app/&scope=user_profile,user_media&response_type=code`;
   useEffect(() => {
     console.log("Search Params", searchParams.get("code"));
+    const getAccessToken = async () => {
+      let obj = {
+        code: searchParams.get("code"),
+      };
+      console.log("OJbje", obj);
+      try {
+        const response = await fetch(`https://bf6e-137-59-1-12.in.ngrok.io`, {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify(obj),
+        });
+        const data = await response.json();
+        const userDetails = await fetch(
+          // `https://graph.instagram.com/17841456185935341?fields=id,username&access_token=${data.access_token}`
+          `https://graph.instagram.com/me/media?fields=id,username,caption&access_token=${data.access_token}`
+        );
+        const user = await userDetails.json();
+        console.log("User media", user);
+      } catch (err) {
+        return err;
+      }
+    };
     getAccessToken();
   }, [searchParams]);
-  const getAccessToken = async () => {
-    let obj = {
-      code: searchParams.get("code"),
-    };
-    console.log("OJbje", obj);
-    try {
-      const response = await fetch(`https://bf6e-137-59-1-12.in.ngrok.io`, {
-        method: "POST",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify(obj),
-      });
-      const data = await response.json();
-      const userDetails = await fetch(
-        // `https://graph.instagram.com/17841456185935341?fields=id,username&access_token=${data.access_token}`
-        `https://graph.instagram.com/me/media?fields=id,caption&access_token=${data.access_token}`
-      );
-      const user = await userDetails.json();
-      console.log("User media", user);
-    } catch (err) {
-      return err;
-    }
-  };
 
   return (
     <>

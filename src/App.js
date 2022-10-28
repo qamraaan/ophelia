09af589cd636b7e248707a68989bcbffbd8538d6
qamraaan <qamraaan@gmail.com>
2022-10-28 10:09:15 +0530

    add search params in useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 function App() {
   // }, []);
--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 const Home = () => {
   const [searchParams] = useSearchParams();
-
-  console.log("Search Params", searchParams.get("code"));
   let url = `https://api.instagram.com/oauth/authorize?client_id=1096816224277958&redirect_uri=https://instadetails.netlify.app/&scope=user_profile,user_media&response_type=code`;
+  useEffect(() => {
+    console.log("Search Params", searchParams.get("code"));
+  }, [searchParams]);
+
   return (
     <>
       <div>

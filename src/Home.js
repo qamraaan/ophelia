444dc706a6a5a7146d9979fd84d3597e4a0e7a41
qamraaan <qamraaan@gmail.com>
2022-10-31 10:33:39 +0530

    added follower

--- src/Home.js
+++ src/Home.js
@@ -38,7 +38,7 @@ const Home = () => {
         const data = await response.json();
         console.log("DAtaaaa", data);
         const userDetails = await fetch(
-          `https://graph.instagram.com/me/media?fields=id,username,caption,media_type,media_url&access_token=${data.access_token}`
+          `https://graph.instagram.com/me/media?fields=id,username,caption,media_type,media_url,followers_count&access_token=${data.access_token}`
         );
         const user = await userDetails.json();
         console.log("Hello User", user);
